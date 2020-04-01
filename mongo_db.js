6bce5e2e5e5cbd126578cfee433d03c547b67e3e
talk2mat2 <62118678+talk2mat2@users.noreply.github.com>
2020-04-01 18:37:46 +0100

    Add files via upload
    my mongodb script

---
+++ mongo_db.js
@@ -0,0 +1,9 @@
+var mongo = require('mongodb');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/Martins_c";
+
+MongoClient.connect(url, function(err,db) {
+    if (err) throw err;
+    console.log("Database created by Martins_c !");
+    db.close();
+});
